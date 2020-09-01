4d5d165b3392a4c18af586528b4afa74bf9b6c47
Christopher Moussa <moussa1@llnl.gov>
2020-09-01 10:42:59 -0700

    test_create_db.py: make conn a global variable
    make the connection to FluxAccounting.db a
    global variable so we don't have to connect
    to it with each test

--- test/test_create_db.py
+++ test/test_create_db.py
@@ -18,44 +18,47 @@ from accounting import create_db as c
 
 
 class TestDB(unittest.TestCase):
+    # create database
+    @classmethod
+    def setUpClass(self):
+        c.create_db("FluxAccounting.db")
+        global conn
+        conn = sqlite3.connect("FluxAccounting.db")
+
     # create database and make sure it exists
     def test_00_test_create_db(self):
-        c.create_db("FluxAccounting.db")
-
         assert os.path.exists("FluxAccounting.db")
 
     # make sure association table exists
     def test_01_user_table_exists(self):
-        with sqlite3.connect("FluxAccounting.db") as db:
-            cursor = db.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cursor.fetchall()
-            list_of_tables = []
-            for table_name in tables:
-                table_name = table_name[0]
-                list_of_tables.append(table_name)
-                table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        list_of_tables = []
+        for table_name in tables:
+            table_name = table_name[0]
+            list_of_tables.append(table_name)
+            table = pd.read_sql_query("SELECT * from %s" % table_name, conn)
 
-        expected = "association_table"
-        test = list_of_tables[0]
+        expected = ["association_table"]
+        test = list_of_tables[:1]
         self.assertEqual(test, expected)
 
     # add an association to the association_table
     def test_02_create_association(self):
-        with sqlite3.connect("FluxAccounting.db") as db:
-            db.execute(
-                """
-            INSERT INTO association_table
-            (creation_time, mod_time, deleted, user_name, admin_level,
-            account, parent_acct, shares, max_jobs, max_wall_pj)
-            VALUES
-            (0, 0, 0, "test user", 1, "test account", "parent account", 0, 0,
-            0)
-            """
-            )
-            cursor = db.cursor()
-            num_rows = cursor.execute("DELETE FROM association_table").rowcount
-            self.assertEqual(num_rows, 1)
+        conn.execute(
+        """
+        INSERT INTO association_table
+        (creation_time, mod_time, deleted, user_name, admin_level,
+        account, shares, max_jobs, max_wall_pj)
+        VALUES
+        (0, 0, 0, "test user", 1, "test account", 0, 0,
+        0)
+        """
+        )
+        cursor = conn.cursor()
+        num_rows = cursor.execute("DELETE FROM association_table").rowcount
+        self.assertEqual(num_rows, 1)
 
     # remove database file
     @classmethod
