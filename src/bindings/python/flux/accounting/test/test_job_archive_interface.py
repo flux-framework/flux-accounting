7abbe4ccfdf81730a981bf68e07f92b68be4394c
Christopher Moussa <moussa1@llnl.gov>
2020-10-13 15:05:30 -0700

    tests: add tests for calc_usage_factor()

--- src/bindings/python/flux/accounting/test/test_job_archive_interface.py
+++ src/bindings/python/flux/accounting/test/test_job_archive_interface.py
@@ -16,8 +16,11 @@ import time
 
 import pandas as pd
 
+from unittest import mock
+
 from flux.accounting import job_archive_interface as jobs
 from flux.accounting import create_db as c
+from flux.accounting import accounting_cli_functions as aclif
 
 
 class TestAccountingCLI(unittest.TestCase):
@@ -52,13 +55,25 @@ class TestAccountingCLI(unittest.TestCase):
             );"""
         )
 
+        c.create_db("FluxAccountingUsers.db")
+        global acct_conn
+        acct_conn = sqlite3.connect("FluxAccountingUsers.db")
+
+        # add bank hierarchy
+        aclif.add_bank(acct_conn, bank="A", shares=1)
+        aclif.add_bank(acct_conn, bank="B", parent_bank="A", shares=1)
+        aclif.add_bank(acct_conn, bank="C", parent_bank="B", shares=1)
+        aclif.add_bank(acct_conn, bank="D", parent_bank="B", shares=1)
+
+        # add users
+        aclif.add_user(acct_conn, username="1001", bank="C")
+        aclif.add_user(acct_conn, username="1002", bank="C")
+        aclif.add_user(acct_conn, username="1003", bank="D")
+        aclif.add_user(acct_conn, username="1004", bank="D")
+
         def populate_job_archive_db(jobs_conn, userid, username, ranks, num_entries):
             nonlocal id
-            nonlocal t_submit
-            nonlocal t_sched
-            nonlocal t_run
-            nonlocal t_cleanup
-            nonlocal t_inactive
+            t_inactive_delta = 2000
 
             for i in range(num_entries):
                 try:
@@ -89,7 +104,7 @@ class TestAccountingCLI(unittest.TestCase):
                             time.time() - 1000,
                             time.time(),
                             time.time() + 1000,
-                            time.time() + 2000,
+                            time.time() + t_inactive_delta,
                             "eventlog",
                             "jobspec",
                             '{"version":1,"execution": {"R_lite":[{"rank":"0","children": {"core": "0"}}]}}',
@@ -102,11 +117,7 @@ class TestAccountingCLI(unittest.TestCase):
                     print(integrity_error)
 
                 id += 1
-                t_submit += 1000
-                t_sched += 1000
-                t_run += 1000
-                t_cleanup += 1000
-                t_inactive += 1000
+                t_inactive_delta += 100
 
         # populate the job-archive DB with fake job entries
         populate_job_archive_db(jobs_conn, 1001, "1001", "0", 2)
@@ -142,28 +153,28 @@ class TestAccountingCLI(unittest.TestCase):
 
     # passing a timestamp after all of the start time
     # of all the completed jobs should return a failure message
-    def test_05_after_start_time_none(self):
+    def test_04_after_start_time_none(self):
         my_dict = {"after_start_time": time.time()}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 0)
 
     # passing a timestamp after the end time of the
     # last job should return all of the jobs
-    def test_06_before_end_time_all(self):
+    def test_05_before_end_time_all(self):
         my_dict = {"before_end_time": time.time() + 10000}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 18)
 
     # passing a timestamp before the end time of
     # all the completed jobs should return a failure message
-    def test_08_before_end_time_none(self):
+    def test_06_before_end_time_none(self):
         my_dict = {"before_end_time": 0}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 0)
 
     # passing a user not in the jobs table
     # should return a failure message
-    def test_09_by_user_failure(self):
+    def test_07_by_user_failure(self):
         my_dict = {"user": "9999"}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 0)
@@ -171,24 +182,166 @@ class TestAccountingCLI(unittest.TestCase):
     # view_jobs_run_by_username() interacts with a
     # passwd file; for the purpose of these tests,
     # just pass the userid
-    def test_10_by_user_success(self):
+    def test_08_by_user_success(self):
         my_dict = {"user": "1001"}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 2)
 
     # passing a combination of params should further
     # refine the query
-    def test_11_multiple_params(self):
+    def test_09_multiple_params(self):
         my_dict = {"user": "1001", "after_start_time": time.time() - 1000}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 2)
 
     # passing no parameters will result in a generic query
     # returning all results
-    def test_15_no_options_passed(self):
+    def test_10_no_options_passed(self):
         my_dict = {}
         job_records = jobs.view_job_records(jobs_conn, op, **my_dict)
         self.assertEqual(len(job_records), 18)
+
+    # users that have run a lot of jobs should have a larger usage factor
+    def test_11_calc_usage_factor_many_jobs(self):
+        user = "1002"
+        bank = "C"
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_0=256 WHERE username='1002' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_1=64 WHERE username='1002' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_2=16 WHERE username='1002' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_3=8 WHERE username='1002' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        acct_conn.commit()
+
+        usage_factor = jobs.calc_usage_factor(
+            jobs_conn, acct_conn, priority_decay_half_life=1, user=user, bank=bank
+        )
+        self.assertEqual(usage_factor, 17044.0)
+
+    # on the contrary, users that have not run a lot of jobs should have
+    # a smaller usage factor
+    def test_12_calc_usage_factor_few_jobs(self):
+        user = "1001"
+        bank = "C"
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_0=4096 WHERE username='1001' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_1=256 WHERE username='1001' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_2=32 WHERE username='1001' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        update_stmt = "UPDATE job_usage_factor_table SET usage_factor_period_3=16 WHERE username='1001' AND bank='C'"
+        acct_conn.execute(update_stmt)
+        acct_conn.commit()
+
+        usage_factor = jobs.calc_usage_factor(
+            jobs_conn, acct_conn, priority_decay_half_life=1, user=user, bank=bank
+        )
+        self.assertEqual(usage_factor, 8500.0)
+
+    # make sure usage factor was written to job_usage_factor_table
+    def test_13_check_usage_factor_in_table(self):
+        select_stmt = "SELECT usage_factor_period_0 FROM job_usage_factor_table WHERE username='1002' AND bank='C'"
+        dataframe = pd.read_sql_query(select_stmt, acct_conn)
+        usage_factor = dataframe.iloc[0]
+        self.assertEqual(usage_factor["usage_factor_period_0"], 17044.0)
+
+    # re-calculating a job usage factor after the end of the last half-life
+    # period should create a new usage bin and update t_half_life_period_table
+    # with the new end time of the current half-life period
+    def test_14_append_jobs_in_diff_half_life_period(self):
+        user = "1001"
+        bank = "C"
+
+        try:
+            jobs_conn.execute(
+                """
+                INSERT INTO jobs (
+                    id,
+                    userid,
+                    username,
+                    ranks,
+                    t_submit,
+                    t_sched,
+                    t_run,
+                    t_cleanup,
+                    t_inactive,
+                    eventlog,
+                    jobspec,
+                    R
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """,
+                (
+                    "200",
+                    "1001",
+                    "1001",
+                    "0",
+                    time.time() + 11604800,
+                    time.time() + 11604900,
+                    time.time() + 11605000,
+                    time.time() + 11605100,
+                    time.time() + 11605200,
+                    "eventlog",
+                    "jobspec",
+                    '{"version":1,"execution": {"R_lite":[{"rank":"0","children": {"core": "0"}}]}}',
+                ),
+            )
+            # commit changes
+            jobs_conn.commit()
+        # make sure entry is unique
+        except sqlite3.IntegrityError as integrity_error:
+            print(integrity_error)
+
+        # re-calculate usage factor for user1001
+        usage_factor = jobs.calc_usage_factor(
+            jobs_conn, acct_conn, priority_decay_half_life=1, user=user, bank=bank
+        )
+        self.assertEqual(usage_factor, 4519.0)
+
+    # simulate a half-life period further; re-calculate
+    # usage for user1001 to make sure its value goes down
+    @mock.patch(
+        "time.time", mock.MagicMock(return_value=(time.time() + (604800 * 2.1)))
+    )
+    def test_15_recalculate_usage_after_half_life_period(self):
+        user = "1001"
+        bank = "C"
+
+        usage_factor = jobs.calc_usage_factor(
+            jobs_conn, acct_conn, priority_decay_half_life=1, user=user, bank=bank
+        )
+
+        self.assertEqual(usage_factor, 2277.00)
+
+    # simulate a half-life period further; assure the new end of the
+    # current half-life period gets updated
+    @mock.patch(
+        "time.time", mock.MagicMock(return_value=(time.time() + (604800 * 2.1)))
+    )
+    def test_16_update_end_half_life_period(self):
+        # fetch timestamp of the end of the current half-life period
+        fetch_half_life_timestamp_query = """
+            SELECT end_half_life_period
+            FROM t_half_life_period_table
+            WHERE cluster='cluster'
+            """
+        dataframe = pd.read_sql_query(fetch_half_life_timestamp_query, acct_conn)
+        old_end_half_life = dataframe.iloc[0]
+
+        jobs.update_end_half_life_period(acct_conn, priority_decay_half_life=1)
+
+        # fetch timestamp of the end of the new half-life period
+        fetch_half_life_timestamp_query = """
+            SELECT end_half_life_period
+            FROM t_half_life_period_table
+            WHERE cluster='cluster'
+            """
+        dataframe = pd.read_sql_query(fetch_half_life_timestamp_query, acct_conn)
+        new_end_half_life = dataframe.iloc[0]
+
+        self.assertGreater(float(new_end_half_life), float(old_end_half_life))
 
     # remove database and log file
     @classmethod
@@ -196,6 +349,7 @@ class TestAccountingCLI(unittest.TestCase):
         jobs_conn.close()
         os.remove("jobs.db")
         os.remove("job_records.csv")
+        os.remove("FluxAccountingUsers.db")
 
 
 def suite():
@@ -206,4 +360,5 @@ def suite():
 
 if __name__ == "__main__":
     from pycotap import TAPTestRunner
+
     unittest.main(testRunner=TAPTestRunner())
