a7a0bd6b57dc64b221db9698652ac462b660ebbd
Christopher Moussa <moussa1@llnl.gov>
2021-01-11 14:51:56 -0800

    tap-driver.py: fix flake8 errors/warnings
    flake8 reported the following warnings/errors:
    
    - E261 at least two spaces before inline comment
    - E226 missing whitespace around arithmetic operator
    - E305 expected 2 blank lines after class or function
    definition, found 1
    
    so fix the errors.

--- config/tap-driver.py
+++ config/tap-driver.py
@@ -5,15 +5,16 @@ import sys
 import os
 from os import path
 
+
 def main():
-    arguments = sys.argv[1:] # 0 is me
+    arguments = sys.argv[1:]  # 0 is me
     try:
-        args_split_point = arguments.index('--')
+        args_split_point = arguments.index("--")
         driver_args = arguments[:args_split_point]
-        test_command = arguments[args_split_point+1:]
+        test_command = arguments[args_split_point + 1 :]
     except ValueError:
         for idx, value in enumerate(arguments):
-            if not value.startswith('--'):
+            if not value.startswith("--"):
                 driver_args = arguments[:idx]
                 test_command = arguments[idx:]
                 break
@@ -21,6 +22,7 @@ def main():
     driver = path.join(path.dirname(path.realpath(__file__)), "tap-driver.sh")
     full_command = [driver] + driver_args + ["--", sys.executable] + test_command
     os.execv(driver, full_command)
+
 
 if __name__ == "__main__":
     main()
