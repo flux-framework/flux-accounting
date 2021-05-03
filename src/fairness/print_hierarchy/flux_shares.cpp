6a0132fedc23d4d1cbaace4a73bfb7f0b3f288df
Christopher Moussa <moussa1@llnl.gov>
2021-05-03 13:30:53 -0700

    flux-shares: use data_writer_stdout subclass
    Now that the weighted_tree library has a data_writer_stdout
    subclass, flux_shares.cpp can make use of its public API to
    print out flux-accounting information.

--- src/fairness/print_hierarchy/flux_shares.cpp
+++ src/fairness/print_hierarchy/flux_shares.cpp
@@ -16,12 +16,10 @@ extern "C" {
 #endif
 }
 
-#include "src/fairness/reader/data_reader_db.hpp"
+#include "src/fairness/writer/data_writer_stdout.hpp"
 
 using namespace Flux::accounting;
-using namespace Flux::reader;
-
-const std::string DBPATH = std::string (X_LOCALSTATEDIR) + "/FluxAccounting.db";
+using namespace Flux::writer;
 
 static void show_usage ()
 {
@@ -35,102 +33,14 @@ static void show_usage ()
               << std::endl;
 }
 
-void print_csv_header (const std::string& delimiter="|")
-{
-    std::cout << "Account" << delimiter
-              << "Username" << delimiter
-              << "RawShares" << delimiter
-              << "RawUsage"
-              << std::endl;
-}
-
-void print_csv (std::shared_ptr<weighted_tree_node_t> node,
-                const std::string& indent,
-                const std::string& delimiter="|")
-{
-
-    if (node == nullptr)
-        return;
-
-    // print node data
-    if (node->is_user ()) {
-        std::cout << indent << node->get_parent ()-> get_name () << delimiter
-                  << node->get_name () << delimiter
-                  << node->get_shares () << delimiter
-                  << node->get_usage ()
-                  << std::endl;
-    } else {
-        std::cout << indent << node->get_name () << delimiter << delimiter
-                  << node->get_shares () << delimiter
-                  << node->get_usage ()
-                  << std::endl;
-    }
-
-    // recur on subtree
-    for (int i = 0; i < node->get_num_children(); i++) {
-        print_csv(node->get_child (i), indent + " ", delimiter);
-    }
-}
-
-void pretty_print_header ()
-{
-    std::cout << std::setw(20) << std::left << "Account"
-              << std::setw(20) << std::right << "Username"
-              << std::setw(20) << std::right << "RawShares"
-              << std::setw(20) << std::right << "RawUsage"
-              << std::endl;
-}
-
-void pretty_print (std::shared_ptr<weighted_tree_node_t> node,
-                   const std::string& indent)
-{
-    std::string name;
-
-    if (node == nullptr)
-        return;
-
-    // print node data
-    if (node->is_user ()) {
-        name = indent + node->get_parent ()->get_name ();
-        std::cout << std::setw(20) << std::left << name
-                  << std::setw(20) << std::right << node->get_name ()
-                  << std::setw(20) << std::right << node->get_shares ()
-                  << std::setw(20) << std::right << node->get_usage ()
-                  << std::endl;
-    } else {
-        name = indent + node->get_name ();
-        std::cout << std::setw(20) << std::left << name
-                  << std::setw(20) << std::right << ""
-                  << std::setw(20) << std::right << node->get_shares ()
-                  << std::setw(20) << std::right << node->get_usage ()
-                  << std::endl;
-    }
-
-    // recur on subtree
-    for (int i = 0; i < node->get_num_children(); i++) {
-        pretty_print(node->get_child (i), indent + " ");
-    }
-}
-
-std::shared_ptr<weighted_tree_node_t> read_from_db (const std::string &filename)
-{
-    data_reader_db_t data_reader;
-    std::shared_ptr<weighted_tree_node_t> root;
-
-    root = data_reader.load_accounting_db (filename);
-    if (root == nullptr)
-        return nullptr;
-
-    return root;
-}
-
 
 int main (int argc, char** argv)
 {
-    bool parsable = false;
-    std::string filepath, delimiter, indent = "";
+    std::string filepath = "";
     int rc = 0;
     std::shared_ptr<weighted_tree_node_t> root;
+    std::string indent, delimiter = "";
+    bool parsable = false;
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
@@ -155,22 +65,8 @@ int main (int argc, char** argv)
         }
     }
 
-    if (filepath == "")
-        filepath = DBPATH;
-
-    root = read_from_db (filepath);
-    if (root == nullptr) {
-        rc = -1;
-        return rc;
-    }
-
-    if (parsable) {
-        print_csv_header (delimiter);
-        print_csv(root, indent, delimiter);
-    } else {
-        pretty_print_header ();
-        pretty_print (root, indent);
-    }
+    data_writer_stdout_t data_writer (indent, parsable, delimiter);
+    rc = data_writer.write_acct_info (filepath, root);
 
     return rc;
 }
