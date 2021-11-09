d0ead3fd2412b5bf19601625df2c155536cba1f7
Christopher Moussa <moussa1@llnl.gov>
2021-11-09 09:02:06 -0800

    t: add queue_table to expected tests

--- src/bindings/python/fluxacct/accounting/test/test_create_db.py
+++ src/bindings/python/fluxacct/accounting/test/test_create_db.py
@@ -54,6 +54,7 @@ class TestDB(unittest.TestCase):
             "job_usage_factor_table",
             "t_half_life_period_table",
             "qos_table",
+            "queue_table",
         ]
         self.assertEqual(list_of_tables, expected)
 
