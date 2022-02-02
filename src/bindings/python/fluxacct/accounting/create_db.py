32160bc05869532e867a67fd340d7768712d131e
Christopher Moussa <moussa1@llnl.gov>
2022-02-02 12:50:16 -0800

    association_table: add max_active_jobs
    Add a new field to the association_table, max_active_jobs,
    which represents the maximum number of pending+running jobs
    a user/bank combo can have at any given time.

--- src/bindings/python/fluxacct/accounting/create_db.py
+++ src/bindings/python/fluxacct/accounting/create_db.py
@@ -98,6 +98,7 @@ def create_db(
                 job_usage        real        DEFAULT 0.0   NOT NULL,
                 fairshare        real        DEFAULT 0.5   NOT NULL,
                 max_running_jobs int(11)     DEFAULT 5     NOT NULL    ON CONFLICT REPLACE DEFAULT 5,
+                max_active_jobs  int(11)     DEFAULT 7     NOT NULL    ON CONFLICT REPLACE DEFAULT 7,
                 qos              tinytext    DEFAULT ''    NOT NULL    ON CONFLICT REPLACE DEFAULT '',
                 PRIMARY KEY   (username, bank)
         );"""
