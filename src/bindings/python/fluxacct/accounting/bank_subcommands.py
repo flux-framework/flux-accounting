6d1df6f388f5039e6aaf886d7a25487412d09d6f
Christopher Moussa <moussa1@llnl.gov>
2023-01-17 08:46:41 -0800

    bank_subcommands: replace print() statements
    Problem: The Python functions in the Accounting class directly emit output to
    STDOUT instead of returning information to the caller of the function. To
    prepare for the transition to using a systemd-managed service that accepts an
    RPC to call the Python functions, this behavior should be changed to return
    information to the caller of the function.
    
    Change the behavior of the bindings that emit output to return any potential
    output instead of print()-ing it to STDOUT. It will now be the responsibility of
    the caller of the function to call the appropriate function, fetch and unpack
    any potential data, and print it out.

--- src/bindings/python/fluxacct/accounting/bank_subcommands.py
+++ src/bindings/python/fluxacct/accounting/bank_subcommands.py
@@ -21,25 +21,28 @@ from fluxacct.accounting import user_subcommands as u
 
 # helper function to print user information in a table format
 def print_user_rows(cur, rows, bank):
-    print("\nUsers Under Bank {bank_name}:\n".format(bank_name=bank))
+    user_str = "\nUsers Under Bank {bank_name}:\n\n".format(bank_name=bank)
     user_headers = [description[0] for description in cur.description]
     # print column names of association_table
     for header in user_headers:
-        print(header.ljust(18), end=" ")
-    print()
+        user_str += header.ljust(18)
+    user_str += "\n"
     for row in rows:
         for col in list(row):
-            print(str(col).ljust(18), end=" ")
-        print()
+            user_str += str(col).ljust(18)
+        user_str += "\n"
+
+    return user_str
 
 
 # helper function to print bank information in a table format
-def print_bank_rows(cur, rows, bank):
+def get_bank_rows(cur, rows, bank):
+    bank_str = ""
     bank_headers = [description[0] for description in cur.description]
     # bank has sub banks, so list them
     for header in bank_headers:
-        print(header.ljust(15), end=" ")
-    print()
+        bank_str += header.ljust(15)
+    bank_str += "\n"
     for row in rows:
         for col in list(row):
             bank_str += str(col).ljust(15)
@@ -54,8 +57,10 @@ def validate_parent_bank(cur, parent_bank):
         row = cur.fetchone()
         if row is None:
             raise ValueError("Parent bank not found in bank table")
+
+        return 0
     except sqlite3.OperationalError as e_database_error:
-        print(e_database_error)
+        return e_database_error
 
 
 ###############################################################
@@ -70,8 +75,13 @@ def add_bank(conn, bank, shares, parent_bank=""):
 
     # if the parent bank is not "", that means the bank trying
     # to be added wants to be placed under an existing parent bank
-    if parent_bank != "":
-        validate_parent_bank(cur, parent_bank)
+    try:
+        if parent_bank != "":
+            validate_parent_bank(cur, parent_bank)
+    except ValueError:
+        return "Parent bank not found in bank table"
+    except sqlite3.OperationalError as e_database_error:
+        return e_database_error
 
     # insert the bank values into the database
     try:
@@ -88,9 +98,11 @@ def add_bank(conn, bank, shares, parent_bank=""):
         )
         # commit changes
         conn.commit()
+
+        return 0
     # make sure entry is unique
     except sqlite3.IntegrityError as integrity_error:
-        print(integrity_error)
+        return integrity_error
 
 
 def view_bank(conn, bank, users=False):
@@ -100,7 +112,7 @@ def view_bank(conn, bank, users=False):
         rows = cur.fetchall()
 
         if rows:
-            print_bank_rows(cur, rows, bank)
+            bank_str = get_bank_rows(cur, rows, bank)
         else:
             return "Bank not found in bank_table"
 
@@ -119,11 +131,14 @@ def view_bank(conn, bank, users=False):
             rows = cur.fetchall()
 
             if rows:
-                print_user_rows(cur, rows, bank)
+                user_str = print_user_rows(cur, rows, bank)
+                bank_str += user_str
             else:
-                print("\nNo users under {bank_name}".format(bank_name=bank))
+                bank_str += "\nNo users under {bank_name}".format(bank_name=bank)
+
+        return bank_str
     except sqlite3.OperationalError as e_database_error:
-        print(e_database_error)
+        return e_database_error
 
 
 def delete_bank(conn, bank):
@@ -159,9 +174,8 @@ def delete_bank(conn, bank):
     # the parent banks, then throw the exception and roll
     # back the changes made to the DB
     except sqlite3.OperationalError as exception:
-        print(exception)
         conn.rollback()
-        return 1
+        return exception
 
     # commit changes
     conn.commit()
