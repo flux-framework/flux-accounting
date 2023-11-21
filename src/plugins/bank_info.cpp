a908833ac0c4132ed6db946bae983db2f685de27
cmoussa1 <moussa1@llnl.gov>
2023-11-21 14:27:48 -0800

    bank_info: add get_user_info () helper function
    Problem: There is some functionality in the priority plugin
    (mf_priority.cpp) that is specific to accessing user_bank_info objects
    in a map data structure. These methods can be abstracted out to the
    class files to clean up some of the plugin code.
    
    Add a new function to the bank_info.cpp class file that handles
    looking up a user/bank combo and fetching a user_bank_info object from
    the users map, or returning NULL on failure.

--- src/plugins/bank_info.cpp
+++ src/plugins/bank_info.cpp
@@ -9,3 +9,29 @@
 \************************************************************/
 
 #include "bank_info.hpp"
+
+user_bank_info* get_user_info (int userid,
+                               char *bank,
+                               std::map<int, std::map<std::string,
+                                                      user_bank_info>> &users,
+                               std::map<int, std::string> &users_def_bank)
+{
+    std::map<std::string, user_bank_info>::iterator bank_it;
+
+    auto it = users.find (userid);
+    if (it == users.end ())
+        return NULL;
+
+    if (bank != NULL) {
+        bank_it = it->second.find (std::string (bank));
+        if (bank_it == it->second.end ())
+            return NULL;
+    } else {
+        bank = const_cast<char*> (users_def_bank[userid].c_str ());
+        bank_it = it->second.find (std::string (bank));
+        if (bank_it == it->second.end ())
+            return NULL;
+    }
+
+    return &bank_it->second;
+}
