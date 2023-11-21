7fc053097b0b4b4c42b5ce94c1af79d16b038569
cmoussa1 <moussa1@llnl.gov>
2023-11-21 14:27:48 -0800

    bank_info: add helper functions for map lookup
    Problem: There is some functionality in the priority plugin
    (mf_priority.cpp) that is specific to accessing user_bank_info objects
    in a map data structure. These methods can be abstracted out to the
    class files to clean up some of the plugin code.
    
    Add two new functions to the bank_info.cpp class file that handle
    looking up a user/bank combo and fetching a user_bank_info object from
    the users map.

--- src/plugins/bank_info.cpp
+++ src/plugins/bank_info.cpp
@@ -9,3 +9,43 @@
 \************************************************************/
 
 #include "bank_info.hpp"
+
+int user_bank_lookup (int userid, char *bank)
+{
+    auto it = users.find (userid);
+    if (it == users.end ()) {
+        return BANK_USER_NOT_FOUND;
+    }
+
+    // make sure user belongs to bank they specified; if no bank was passed in,
+    // look up their default bank
+    if (bank != NULL) {
+        auto bank_it = it->second.find (std::string (bank));
+        if (bank_it == it->second.end ())
+            return BANK_INVALID;
+    } else {
+        bank = const_cast<char*> (users_def_bank[userid].c_str ());
+        auto bank_it = it->second.find (std::string (bank));
+        if (bank_it == it->second.end ())
+            return BANK_NO_DEFAULT;
+    }
+
+    return BANK_SUCCESS;
+}
+
+
+user_bank_info get_user_info (int userid, char *bank)
+{
+    std::map<std::string, user_bank_info>::iterator bank_it;
+
+    auto it = users.find (userid);
+
+    if (bank != NULL) {
+        bank_it = it->second.find (std::string (bank));
+    } else {
+        bank = const_cast<char*> (users_def_bank[userid].c_str ());
+        bank_it = it->second.find (std::string (bank));
+    }
+
+    return bank_it->second;
+}
--- src/plugins/bank_info.hpp
+++ src/plugins/bank_info.hpp
@@ -15,6 +15,8 @@
 
 #include <vector>
 #include <string>
+#include <map>
+#include <iterator>
 
 // all attributes are per-user/bank
 class user_bank_info {
@@ -30,5 +32,29 @@ public:
     int queue_factor;                // priority factor associated with queue
     int active;                      // active status
 };
+
+// different codes to return as a result of looking up user/bank information:
+//
+// BANK_SUCCESS: we found an entry for the passed-in user/bank
+// BANK_USER_NOT_FOUND: the user could not be found in the plugin map
+// BANK_INVALID: the user specified a bank they don't belong to
+// BANK_NO_DEFAULT: the user does not have a default bank in the plugin map
+enum bank_info_codes {
+    BANK_SUCCESS,
+    BANK_USER_NOT_FOUND,
+    BANK_INVALID,
+    BANK_NO_DEFAULT
+};
+
+// these data structures are defined in the priority plugin
+extern std::map<int, std::map<std::string, user_bank_info>> users;
+extern std::map<int, std::string> users_def_bank;
+
+// check if a user has an entry in the users map
+int user_bank_lookup (int userid, char *bank);
+
+// get a user_bank_info object that points to user/bank
+// information in users map
+user_bank_info get_user_info (int userid, char *bank);
 
 #endif // BANK_INFO_H
