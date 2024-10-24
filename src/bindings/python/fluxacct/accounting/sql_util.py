663d282c26023cafbec0d4bbe972611988e42b4d
Christopher Moussa <moussa1@llnl.gov>
2024-10-24 13:39:46 -0700

    python: add SQLite utitlity file
    Problem: the flux-accounting Python bindings have lots of separate
    (and probably duplicate) helper code for validating and interacting
    with the SQLite database. It would be cleaner and easier to maintain if
    it was centralized in one file that could be referenced throughout the
    bindings.
    
    Add a new SQL utility file to the Python bindings that will have helper
    functions for interacting with the SQLite database. Add a function to
    this file that will validate a custom list of table column names
    against a valid list of table column names.

---
+++ src/bindings/python/fluxacct/accounting/sql_util.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+###############################################################
+# Copyright 2024 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+###############################################################
+def validate_columns(columns, valid_columns):
+    """
+    Validate a list of of columns against a list of valid columns of a table
+    in a flux-accounting database.
+
+    Args:
+        columns: a list of column names
+        valid_columns: a list of valid column names
+
+    Raises:
+        ValueError: at least one of the columns passed in is not valid
+    """
+    invalid_columns = [column for column in columns if column not in valid_columns]
+    if invalid_columns:
+        raise ValueError(f"invalid fields: {', '.join(invalid_columns)}")
