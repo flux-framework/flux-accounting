5067b6456b52b6107ca94a375155237426daeecd
cmoussa1 <moussa1@llnl.gov>
2024-11-12 08:52:16 -0800

    db_info_subcommands: handle more tables
    Problem: The export-db and pop-db commands only handle a couple of
    columns from just the association_table and bank_table. These commands
    would be more robust if they included export and import support from all
    of the tables.
    
    Restructure the export_db_info() function to return data from *all* of
    the tables in the flux-accounting DB into separate .csv files, labeled
    with their table names in the database.
    
    Restructure the populate_db() function to handle populating any of the
    tables in the flux-accounting DB with a corresponding .csv file. Add an
    optional argument to the function to allow the user to specify which
    columns to include from the file when populating the table.

--- src/cmd/flux-account-service.py
+++ src/cmd/flux-account-service.py
@@ -549,17 +549,17 @@ class AccountingService:
 
     def export_db(self, handle, watcher, msg, arg):
         try:
-            val = d.export_db_info(
-                self.conn,
-                msg.payload["users"],
-                msg.payload["banks"],
-            )
+            val = d.export_db_info(self.conn)
 
             payload = {"export_db": val}
 
             handle.respond(msg, payload)
         except KeyError as exc:
             handle.respond_error(msg, 0, f"missing key in payload: {exc}")
+        except sqlite3.OperationalError as exc:
+            handle.respond_error(msg, 0, f"a SQLite error occurred: {exc}")
+        except IOError as exc:
+            handle.respond_error(msg, 0, f"an IO error occurred: {exc}")
         except Exception as exc:
             handle.respond_error(
                 msg, 0, f"a non-OSError exception was caught: {str(exc)}"
@@ -569,8 +569,8 @@ class AccountingService:
         try:
             val = d.populate_db(
                 self.conn,
-                msg.payload["users"],
-                msg.payload["banks"],
+                msg.payload["csv_file"],
+                msg.payload["fields"],
             )
 
             payload = {"pop_db": val}
@@ -578,6 +578,12 @@ class AccountingService:
             handle.respond(msg, payload)
         except KeyError as exc:
             handle.respond_error(msg, 0, f"missing key in payload: {exc}")
+        except sqlite3.OperationalError as exc:
+            handle.respond_error(msg, 0, f"a SQLite error occurred: {exc}")
+        except IOError as exc:
+            handle.respond_error(msg, 0, f"an IO error occurred: {exc}")
+        except ValueError as exc:
+            handle.respond_error(msg, 0, f"{exc}")
         except Exception as exc:
             handle.respond_error(
                 msg, 0, f"a non-OSError exception was caught: {str(exc)}"
