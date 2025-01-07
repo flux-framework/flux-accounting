b2127a1441f09abf27dc3d7b081a12b24b19faff
Christopher Moussa <moussa1@llnl.gov>
2025-01-07 13:40:13 -0800

    plugin: copy over jj.* code
    Problem: flux-core has some useful code for counting the resources for
    a job, but it needs to be manually copied over.
    
    Copy the jj.* files from flux-core and place them in the same folder as
    the priority plugin so that it can be compiled together and have its
    methods able to be accessed by the plugin.
    
    Manually copy over the streq() definition and place it in jj.hpp as
    well as copy over its LICENSE from flux-core. Reference it in jj.hpp.

---
+++ src/plugins/jj.cpp
@@ -0,0 +1,165 @@
+/************************************************************\
+ * Copyright 2014 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+extern "C" {
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <errno.h>
+#include <string.h>
+#include <jansson.h>
+
+#include "jj.hpp"
+
+static int jj_read_level (json_t *o, int level, struct jj_counts *jj);
+
+static int jj_read_vertex (json_t *o, int level, struct jj_counts *jj)
+{
+    int count;
+    const char *type = NULL;
+    json_t *with = NULL;
+    json_error_t error;
+    int exclusive = 0;
+
+    if (json_unpack_ex (o, &error, 0, "{ s:s s:i s?b s?o }",
+                       "type", &type,
+                       "count", &count,
+                       "exclusive", &exclusive,
+                       "with", &with) < 0) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                  "level %d: %s", level, error.text);
+        errno = EINVAL;
+        return -1;
+    }
+    if (count <= 0) {
+        sprintf (jj->error, "Invalid count %d for type '%s'",
+                            count, type);
+        errno = EINVAL;
+        return -1;
+    }
+    if (streq (type, "node")) {
+        jj->nnodes = count;
+        if (exclusive)
+            jj->exclusive = true;
+    }
+    else if (streq (type, "slot"))
+        jj->nslots = count;
+    else if (streq (type, "core"))
+        jj->slot_size = count;
+    else if (streq (type, "gpu"))
+        jj->slot_gpus = count;
+    else {
+        sprintf (jj->error, "Unsupported resource type '%s'", type);
+        errno = EINVAL;
+        return -1;
+    }
+    if (with)
+        return jj_read_level (with, level+1, jj);
+    return 0;
+
+}
+
+static int jj_read_level (json_t *o, int level, struct jj_counts *jj)
+{
+    int i;
+    json_t *v = NULL;
+
+    if (!json_is_array (o)) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                  "level %d: must be an array", level);
+        errno = EINVAL;
+        return -1;
+    }
+    json_array_foreach (o, i, v) {
+        if (jj_read_vertex (v, level, jj) < 0)
+            return -1;
+    }
+    return 0;
+}
+
+int jj_get_counts (const char *spec, struct jj_counts *jj)
+{
+    json_t *o = NULL;
+    json_error_t error;
+    int rc = -1;
+
+    if ((o = json_loads (spec, 0, &error)) == NULL) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                  "JSON load: %s", error.text);
+        errno = EINVAL;
+        return -1;
+    }
+
+    rc = jj_get_counts_json (o, jj);
+    json_decref (o);
+    return rc;
+}
+
+int jj_get_counts_json (json_t *jobspec, struct jj_counts *jj)
+{
+    int version;
+    json_t *resources = NULL;
+    json_error_t error;
+
+    if (!jj) {
+        errno = EINVAL;
+        return -1;
+    }
+    memset (jj, 0, sizeof (*jj));
+
+    if (json_unpack_ex (jobspec, &error, 0, "{s:i s:o}",
+                        "version", &version,
+                        "resources", &resources) < 0) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                  "at top level: %s", error.text);
+        errno = EINVAL;
+        return -1;
+    }
+    if (version != 1) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                 "Invalid version: expected 1, got %d", version);
+        errno = EINVAL;
+        return -1;
+    }
+    /* N.B. attributes.system is generally optional, but
+     * attributes.system.duration is required in jobspec version 1 */
+    if (json_unpack_ex (jobspec, &error, 0, "{s:{s:{s:F}}}",
+                        "attributes",
+                          "system",
+                            "duration", &jj->duration) < 0) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                  "at top level: getting duration: %s", error.text);
+        errno = EINVAL;
+        return -1;
+    }
+    if (jj_read_level (resources, 0, jj) < 0)
+        return -1;
+
+    if (jj->nslots <= 0) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                 "Unable to determine slot count");
+        errno = EINVAL;
+        return -1;
+    }
+    if (jj->slot_size <= 0) {
+        snprintf (jj->error, sizeof (jj->error) - 1,
+                 "Unable to determine slot size");
+        errno = EINVAL;
+        return -1;
+    }
+    if (jj->nnodes)
+        jj->nslots *= jj->nnodes;
+    return 0;
+}
+
+}
+/* vi: ts=4 sw=4 expandtab
+ */
