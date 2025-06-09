6ad94c1e97e0891b3d811a3f5c80c2129cd57c5a
Christopher Moussa <moussa1@llnl.gov>
2025-06-09 12:52:19 -0700

    cmd: move update-usage to its own script
    Problem: The update-usage command is a part of the flux-accounting
    service, which means that when it is running (especially on a large
    flux-accounting DB that can take a lot of time), it will halt the
    execution of other flux-accounting commands (even ones that just read
    from the DB) until the update-usage command finishes running. This
    command should be moved to its own script that establishes a separate
    connection to the DB so other flux-accounting commands can run while
    job usage values are being updated.
    
    Create a new Python command script in the src/cmd/ directory called
    flux-account-update-usage.py, a script that will just call
    update_job_usage(), similar to what the update-usage command in the
    flux-accounting service was doing.

---
+++ src/cmd/flux-account-update-usage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+###############################################################
+# Copyright 2025 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+###############################################################
+import logging
+import sqlite3
+import argparse
+import sys
+import os
+
+import fluxacct.accounting
+from fluxacct.accounting import job_usage_calculation as job_usage
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+LOGGER = logging.getLogger(__name__)
+
+
+def set_db_loc(args):
+    path = args.path if args.path else fluxacct.accounting.DB_PATH
+
+    return path
+
+
+def est_sqlite_conn(path):
+    # try to open database file; will exit with -1 if database file not found
+    if not os.path.isfile(path):
+        print(f"Database file does not exist: {path}", file=sys.stderr)
+        sys.exit(1)
+
+    db_uri = "file:" + path + "?mode=rw"
+    try:
+        conn = sqlite3.connect(db_uri, uri=True)
+        # set foreign keys constraint
+        conn.execute("PRAGMA foreign_keys = 1")
+    except sqlite3.OperationalError as exc:
+        print(f"Unable to open database file: {db_uri}", file=sys.stderr)
+        print(f"Exception: {exc}")
+        sys.exit(1)
+
+    return conn
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="""
+        Description: Send a bulk update of user information from a
+        flux-accounting database to the multi-factor priority plugin.
+        """
+    )
+
+    parser.add_argument(
+        "-p", "--path", dest="path", help="specify location of database file"
+    )
+    parser.add_argument(
+        "--priority-decay-half-life",
+        default=1,
+        type=int,
+        help="number of weeks for a job's usage contribution to a half-life decay",
+        metavar="PRIORITY DECAY HALF LIFE",
+    )
+    args = parser.parse_args()
+
+    path = set_db_loc(args)
+    conn = est_sqlite_conn(path)
+
+    job_usage.update_job_usage(conn, args.priority_decay_half_life)
+
+
+if __name__ == "__main__":
+    main()
