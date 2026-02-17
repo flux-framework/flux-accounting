e6187c09181790d07af171e2d515ed652772d65c
Christopher Moussa <moussa1@llnl.gov>
2026-02-17 10:58:59 -0800

    plugin: add held job ID initialization
    Problem: In check_and_release_held_jobs (), held_job_id is not set in
    the case where removing the max_sched_jobs dependency fails, which is
    inconsistent with the other checks throughout this function.
    
    Add the initialization of held_job_id in the if-branch check for
    removing the max_sched_jobs dependency from a held job.

--- src/plugins/mf_priority.cpp
+++ src/plugins/mf_priority.cpp
@@ -348,6 +348,7 @@ static int check_and_release_held_jobs (flux_plugin_t *p, Association *b)
                                                held_job.id,
                                                D_ASSOC_MSJ) < 0) {
                 dependency = D_ASSOC_MSJ;
+                held_job_id = held_job.id;
                 goto error;
             }
             held_job.remove_dep (D_ASSOC_MSJ);
