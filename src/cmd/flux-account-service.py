4d667c7a3771ac1c4aeea52dc8ecdf3b231120cf
Sam Maloney <56830868+sam-maloney@users.noreply.github.com>
2026-05-21 14:54:22 +0200

    commands: add missing shebangs
    Problem: `flux-account.py` and `flux-account-service.py` are executable
    but have no shebangs. This can lead to the executable bit being stripped
    during RPM builds.
    
    Add shebangs to match other Python command scripts.

--- src/cmd/flux-account-service.py
+++ src/cmd/flux-account-service.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ###############################################################
 # Copyright 2022 Lawrence Livermore National Security, LLC
 # (c.f. AUTHORS, NOTICE.LLNS, COPYING)
