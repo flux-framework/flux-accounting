44be9b356a2d39af2a29f3b031a9d690a4e37100
Christopher Moussa <moussa1@llnl.gov>
2026-06-23 10:05:53 -0700

    update_usage: use raw decay half-life parameter
    Problem: There are a couple places in job_usage_calculation.py where the
    PriorityDecayHalfLife parameter is assumed to be in units of weeks, but
    this has since been changed to be in units of seconds.
    
    Remove the assumption that this parameter is in units of weeks by
    removing the multiplication of its value by 604800.

--- src/bindings/python/fluxacct/accounting/job_usage_calculation.py
+++ src/bindings/python/fluxacct/accounting/job_usage_calculation.py
@@ -162,7 +162,7 @@ def calc_usage_factor(
     usage_factors = [row[1] for row in period_rows]
 
     # hl_period represents the number of seconds that represent one usage bin
-    hl_period = pdhl * 604800
+    hl_period = pdhl
 
     last_t_inactive = 0.0
     usg_current = 0.0
@@ -218,7 +218,7 @@ def calc_usage_factor(
 
 
 def check_end_hl(acct_conn, pdhl):
-    hl_period = pdhl * 604800
+    hl_period = pdhl
 
     cur = acct_conn.cursor()
 
